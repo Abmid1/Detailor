68c9b8f80d895bec5c194c5562ef98c6adc0c7e6
abmid1 <tipagya518@gmail.com>
2026-05-18 14:22:54 +0000

    tech/index

--- technician-app/app/_layout.tsx
+++ technician-app/app/_layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout() {
     <>
       <StatusBar style="light" />
       <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#0A0A0F' } }}>
+        <Stack.Screen name="index" />
         <Stack.Screen name="auth" />
         <Stack.Screen name="tabs" />
         <Stack.Screen name="job" options={{ animation: 'slide_from_right' }} />
